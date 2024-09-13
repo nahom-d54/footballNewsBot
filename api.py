ad842a09bf378fd3ba915a2b903d6bcb0b3d41e2
Nahom <nahom.d54@gmail.com>
2024-09-13 22:36:52 +0300

    added logger for verbose logging

--- api.py
+++ api.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI, Request
-from app.utils.validator import UpdateModel
-from typing import Optional
 from telegram import Update
 from app.config.bot import app as bot_app
 from app.config.enviroments import Env
+import logging
 
 
 
 app = FastAPI()
+logger = logging.Logger("Bot logger")
 
 
 @app.post('/tg-webhook')
 async def webhook(request: Request):
     update_data = await request.json()
+    logger.info("Update: ")
+    logger.info(update_data)
     telegram_update = Update.de_json(update_data, bot_app.bot)
     await bot_app.process_update(telegram_update)
     return {"status": True}
@@ -23,7 +25,8 @@ async def set_webhook(request: Request):
     if not (Env.SECRET_KEY == secret_key):
         return {"status": False, "message": "Invalid Secret Key"}
     result = await bot_app.bot.set_webhook(request.query_params.get('webhook_url'))
-    return {"message": "webhook set", "status": False, "result": result}
+    logger.info(result)
+    return {"message": "webhook set", "status": True, "result": result}
 
 @app.get('/')
 async def index(request: Request):
