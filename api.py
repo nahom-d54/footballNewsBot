879167153094651b3b9b189023a04998edb2b5ad
Nahom <nahom.d54@gmail.com>
2024-09-14 10:55:32 +0300

    test 4

--- api.py
+++ api.py
@@ -7,17 +7,23 @@ import logging
 from contextlib import asynccontextmanager
 
 
-
-logger = logging.Logger("Bot logger")
+logging.basicConfig(level=logging.INFO)
 
 
     
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with bot_app:
-        await bot_app.start()
-        yield
-        await bot_app.stop()
+    logging.info("Initializing PTB application...")
+    if not bot_app._initialized:
+        await bot_app.initialize()
+
+    logging.info("PTB application initialized.")
+    yield
+
+    
+    logging.info("Shutting down PTB application...")
+    await bot_app.shutdown()
+    logging.info("PTB application shut down.")
     
     
 app = FastAPI(lifespan=lifespan)
@@ -25,11 +31,11 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post('/tg-webhook')
 async def webhook(request: Request):
-    # if not bot_app._initialized:
-    #     await bot_app.initialize()
+    if not bot_app._initialized:
+        await bot_app.initialize()
     update_data = await request.json()
-    logger.info("Update: ")
-    logger.info(update_data)
+    logging.info("Update: ")
+    logging.info(update_data)
     telegram_update = Update.de_json(update_data, bot_app.bot)
     await bot_app.process_update(telegram_update)
     return Response({"status": True}, status_code=HTTPStatus.OK)
@@ -41,7 +47,7 @@ async def set_webhook(request: Request):
         return {"status": False, "message": "Invalid Secret Key"}
     await bot_app.bot.delete_webhook()
     result = await bot_app.bot.set_webhook(request.query_params.get('webhook_url'))
-    logger.info(result)
+    logging.info(result)
     return {"message": "webhook set", "status": True, "result": result}
 
 @app.get('/')
--- app/config/bot.py
+++ app/config/bot.py
@@ -1,11 +1,11 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Application
+from telegram.ext import ApplicationBuilder
 from ..controllers import commands
 from .enviroments import Env
 
 #app = ApplicationBuilder().token(Env.TELEGRAM_BOT_TOKEN).build()
 app = (
-    Application.builder()
+    ApplicationBuilder()
     .updater(None)
     .token(Env.TELEGRAM_BOT_TOKEN) # replace <your-bot-token>
     .read_timeout(7)
